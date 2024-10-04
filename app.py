e61c8bce5e9d0f235b7d16421b84e250a4303476
DJJJNabba <jjemailforgames99@gmail.com>
2024-10-04 18:27:44 +1000

    app update

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "test"
+    return render_template('index.html')
+
+@app.route('/AtomicDawn')
+def atomic_dawn():
+    return render_template('AtomicDawn.html')
+
+@app.route('/Certificate')
+def certificate():
+    return render_template('Certificate.html')
+
+@app.route('/KeyGenie')
+def key_genie():
+    return render_template('KeyGenie.html')
+
+@app.route('/MUMC')
+def mumc():
+    return render_template('mumc.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
